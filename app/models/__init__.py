004c908732efc1a632ed2f31fce1fa80752254a4
Mahmoud Saqr <mahmoud.saqrr25@gmail.com>
2026-06-26 09:32:25 +0300

    Setup `alembic` for db migrations
    Create initial database migration

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.agent import Agent
-from app.models.audio_file import AudioFile
-from app.models.chat_session import ChatSession
-from app.models.message import Message
-
-__all__ = ["Agent", "AudioFile", "ChatSession", "Message"]
