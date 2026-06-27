7eb93f3b5778a0a4bb76e4fb3b9f7a6fd7dad443
Mahmoud Saqr <mahmoud.saqrr25@gmail.com>
2026-06-27 12:04:20 +0300

    Refactor configs
    - Add default sqlite3 database url
    - Add configs for openai tts/stt

--- app/core/config.py
+++ app/core/config.py
@@ -9,12 +9,17 @@ class Settings(BaseSettings):
     APP_VERSION: str = "1.0.0"
     LOG_LEVEL: str = "INFO"
 
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite+aiosqlite:///./app.db"
+
     OPENAI_API_KEY: SecretStr | None = None
     OPENAI_MODEL_NAME: str = "gpt-4o-mini"
+    OPENAI_STT_MODEL: str = "whisper-1"
+    OPENAI_TTS_MODEL: str = "tts-1"
+    OPENAI_TTS_VOICE: str = "alloy"
+    OPENAI_TTS_OUTPUT_MIME: str = "audio/mpeg"
 
     AUDIO_STORAGE_DIR: str = "audio_files"
     MAX_AUDIO_FILE_SIZE: int = 10 * 1024 * 1024
 
 
-settings = Settings()  # ty: ignore[missing-argument]
+settings = Settings()
